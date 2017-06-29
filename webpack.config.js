a03ad292439221e82d310c664cab1431a2f9631f
colinjlacy <colin@webcake.co>
2017-06-29 17:50:15 -0500

    fixes loader options

--- webpack.config.js
+++ webpack.config.js
@@ -46,10 +46,15 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
-				loader: 'babel-loader'
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015']
+				}
 			},
 			{
 				test: /\.ts$/,
+				exclude: /node_modules/,
 				loader: 'ts-loader'
 			},
 			{
