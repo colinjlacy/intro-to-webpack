b3d8cc7ef30d6ba255f2455e371b453aed8bc729
colinjlacy <colin@webcake.co>
2017-06-29 17:33:50 -0500

    excludes node modules

--- webpack.config.js
+++ webpack.config.js
@@ -39,10 +39,12 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
+				exclude: /node_modules/,
 				loader: 'babel-loader'
 			},
 			{
 				test: /\.ts$/,
+				exclude: /node_modules/,
 				loader: 'ts-loader'
 			}
 		]
